4ac490f03c7cee56554dfb24975ce472e64dfa67
Cody Partington <codypartington@Codys-Macbook-Pro.local>
2022-02-24 22:53:34 +1000

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+`use strict`
+
+window.onload=function(){
+  let year = new Date().getFullYear();
+
+  document.getElementById(
+    `footer`
+  ).textContent = `© Cody Partington ${year}`;
+
+  const navButton = document.querySelector(`.menu-toggle`);
+  const navPosition = document.querySelector(`.navigation`);
+
+  navButton.addEventListener(`click`, function () {
+    const inMenu = navPosition.getAttribute(`data-visible`);
+    
+    if (inMenu == `false`) {
+      navPosition.setAttribute(`data-visible`, true)
+    } else {navPosition.setAttribute(`data-visible`, false);}
+  });
+}
+
+const teacherOne = `Matt`
+const teacherTwo = `Glen`
+
+console.log(`Easter egg for the ${teacherOne} or ${teacherTwo}`)
