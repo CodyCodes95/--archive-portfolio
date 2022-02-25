c5f5b8b76926591d36f5d6100c582238d7828a5c
Cody Partington <codypartington@Codys-Macbook-Pro.local>
2022-02-25 13:34:28 +1000

    Added all pages. Content added to about-me

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-`use strict`
+`use strict`;
 
-window.onload=function(){
+window.onload = function () {
   let year = new Date().getFullYear();
 
-  document.getElementById(
-    `footer`
-  ).textContent = `© Cody Partington ${year}`;
+  document.getElementById(`footer`).textContent = `© Cody Partington ${year}`;
 
   const navButton = document.querySelector(`.menu-toggle`);
   const navPosition = document.querySelector(`.navigation`);
 
   navButton.addEventListener(`click`, function () {
     const inMenu = navPosition.getAttribute(`data-visible`);
-    
+
     if (inMenu == `false`) {
-      navPosition.setAttribute(`data-visible`, true)
-    } else {navPosition.setAttribute(`data-visible`, false);}
+      navPosition.setAttribute(`data-visible`, true);
+    } else {
+      navPosition.setAttribute(`data-visible`, false);
+    }
   });
-}
+};
 
-const teacherOne = `Matt`
-const teacherTwo = `Glen`
+const teacherOne = `Matt`;
+const teacherTwo = `Glen`;
 
-console.log(`Easter egg for the ${teacherOne} or ${teacherTwo}`)
+console.log(`Easter egg for the ${teacherOne} or ${teacherTwo}`);
